0a1bd572a469448d9fe0bd5f8d8fe726e16f348c
detroitpro <eric@polerecky.com>
2013-12-18 09:41:13 -0500

    moved projects into folders that match namespace - more .net-ish

---
+++ Tests/BitcoinSharp.Tests.Unit/TestConfig.cs
@@ -0,0 +1,20 @@
+﻿using log4net.Config;
+using NUnit.Framework;
+
+namespace BitCoinSharp.Tests.Unit
+{
+    [SetUpFixture]
+    public class TestConfig
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+    }
+}
